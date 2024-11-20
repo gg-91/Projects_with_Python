363c11a348ec89a9edbed36da9632fb64140e946
Gaurav Grewal <177267519+gg-91@users.noreply.github.com>
2024-11-20 00:35:49 -0800

    Update webscrape_with_Parsera.py
    added docstring

--- webscrape_with_Parsera.py
+++ webscrape_with_Parsera.py
@@ -1,7 +1,7 @@
-# Using a new lightweight python library called Parsera that utilizes LLMs to scrape websites making web scraping a very easy 
-# & straight-forward task. I scraped a few different websites and pulled the results in a json file with ease using pandas. I 
-# needed OpenAI API key & some usage credit to make requests. Parsera pulls all the data and uses LLM(gpt-4o in this case) to
-# figure out the columns requested. The library works with dynamic websites & can scroll pages as well.
+"""Using a new lightweight python library called Parsera that utilizes LLMs to scrape websites making web scraping a very easy 
+& straight-forward task. I scraped a few different websites and pulled the results in a json file with ease using pandas. I 
+needed OpenAI API key & some usage credit to make requests. Parsera pulls all the data and uses LLM(gpt-4o in this case) to
+figure out the columns requested. The library works with dynamic websites & can scroll pages as well."""
 
 import os
 from parsera import Parsera
