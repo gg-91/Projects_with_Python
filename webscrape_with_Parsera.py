6857244a3d86da8b034c97cf6aa9d3fddddfd50f
Gaurav Grewal <177267519+gg-91@users.noreply.github.com>
2024-11-19 17:00:46 -0800

    Update webscrape_with_Parsera.py
    Made some url updates and added description at top

--- webscrape_with_Parsera.py
+++ webscrape_with_Parsera.py
@@ -1,14 +1,21 @@
+# Using a new lightweight python library called Parsera that utilizes LLMs to scrape websites making web scraping a very easy 
+# & straight-forward task. I scraped a few different websites and pulled the results in a json file with ease using pandas. I 
+# needed OpenAI API key & some usage credit to make requests. Parsera pulls all the data and uses LLM(gpt-4o in this case) to
+# figure out the columns requested. The library works with dynamic websites & can scroll pages as well.
+
 import os
 from parsera import Parsera
 import pandas as pd
 
 
-url = "https://www.indeed.com/jobs?q=data+analyst&l=San+Francisco%2C+CA"
+url = "https://finance.yahoo.com/markets/world-indices/"
 elements = {
-    "Title": "Job title",
-    "Company": "Company name",
-    "Location": "Job location",
-    "Full job description": "Job Description",
+    "Symbol": "Symbol",
+    "Name": "Stock Name",
+    "Price": "Price",
+    "Change": "Change",
+    "Change%": "Change%",
+    "Volume": "Volume",
 }
 
 scraper = Parsera()
@@ -17,4 +24,4 @@ result = scraper.run(url=url, elements=elements)
 df = pd.DataFrame(result)
 print(df)
 
-df_json = df.to_json("Indeed-out.json", orient="records", lines=True)
+df_json = df.to_json("yahoo-output.json", orient="records", lines=True)
